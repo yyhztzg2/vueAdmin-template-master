4bccaea531f00a84a134dab0bef7f8d1b05407cb
yyhztzg2 <yyhztzg2@qq.com>
2020-11-17 05:46:13 +0800

    2

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -44,7 +44,7 @@ export default {
     return {
       list: null,
       listLoading: true,
-      asasasa:'aaaaaaa22122aaaaa'
+      asasasa:'aaaaaaa22122222222222aaaaa'
     }
   },
   filters: {
