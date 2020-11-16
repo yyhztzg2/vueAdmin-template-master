a584f0683a9981b8d2816929c4de39dbaa370d5b
yyhztzg2 <yyhztzg2@qq.com>
2020-11-17 05:38:20 +0800

    2

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -11,7 +11,7 @@
           {{scope.row.title}}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="Author" width="111" align="center">
         <template slot-scope="scope">
           <span>{{scope.row.author}}</span>
         </template>
